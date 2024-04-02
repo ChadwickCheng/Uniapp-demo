30773a8528e837355ae7ae8ce42a881f45f9892e
Dan Cheng <ChadwickCheng@outlook.com>
2024-04-02 13:47:01 +0800

    开始day2

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,12 @@
 <template>
+  <CustomNavbar />
   <uni-card>
     <text>这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
   </uni-card>
 </template>
 
 <script setup lang="ts">
-//
+import CustomNavbar from './Components/CustomNavbar.vue'
 </script>
 
 <style lang="scss">
