bcb09848dc32e6fb9143b44699c80a305e7e3319
Dan Cheng <ChadwickCheng@outlook.com>
2024-04-03 19:27:22 +0800

    热门推荐数据获取

---
+++ src/services/hot.ts
@@ -0,0 +1,18 @@
+import { http } from '@/utils/http'
+import type { PageParams } from '@/types/global'
+
+type HotParams = PageParams & { subType?: string }
+/**
+ * 通用热门推荐类型
+ * @param url 请求地址
+ * @param data 请求参数
+ */
+const getHotRecommendAPI = (url: string, data?: HotParams) => {
+  return http({
+    method: 'GET',
+    url,
+    data,
+  })
+}
+
+export { getHotRecommendAPI }
